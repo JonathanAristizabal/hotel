c1ac2d80a46f711bc47aa4449bf71edcb1cc9c5f
JonathanAristizabal <116238646+JonathanAristizabal@users.noreply.github.com>
2023-08-22 21:04:26 -0500

    Add files via upload

---
+++ panel_gestor.php
@@ -0,0 +1,180 @@
+<?php
+include 'conections/conection.php';
+
+// Consulta SQL para obtener los datos de la tabla 'usuarios'
+$sql = "SELECT * FROM usuarios";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/panel_gestor.css">
+    <title>panel_gestor</title>
+</head>
+
+<body>
+    <!-- encabezado -->
+    <header>
+        <div id="logo">
+            <img src="img/logoclaro.png" alt="Logo del Hotel" width="135px" height="70px">
+        </div>
+        <h1>Panel de gestión</h1>
+        <nav class="navegacion">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="logout.php">Cerrar sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+    <nav class="panel">
+        <!-- Menu de navegación a los módulos -->
+        <ul>
+            <li class="consultar-item"><a href="#consultar" id="consultar-link">Consultar</a></li>
+            <br>
+            <br>
+            <li class="facturacion-item"><a href="#facturacion" id="facturacion-link">Facturación</a></li>
+            <li class="habitaciones-item"><a href="#habitaciones" id="habitaciones-link">Habitaciones</a></li>
+            <li class="huespedes-item"><a href="#huespedes" id="huespedes-link">Huéspedes</a></li>
+            <li class="pedidos-item"><a href="#pedidos" id="pedidos-link">Pedidos</a></li>
+            <li class="usuarios-item"><a href="#usuarios" id="usuarios-link">Usuarios</a></li>
+            <br>
+            <br>
+            <li class="configuracion-item"><a href="#configuracion" id="configuracion-link">Configuración</a></li>
+
+        </ul>
+    </nav>
+
+    <main>
+        <!-- Módulo de consultar -->
+        <section class="modulo" id="consultar">
+            <div class="modulo-header">Consultar</div>
+            <div class="modulo-content">
+                <!-- Contenido del módulo de consultar... -->
+                <form id="consulta-form">
+                    <input type="text" id="cedula" name="cedula" placeholder="Numero de documento">
+                    <br>
+                    <br>
+                    <button type="submit">Consultar</button>
+                </form>
+                <div id="resultados">
+                    <!-- Aquí se mostrarán los resultados de la consulta -->
+                </div>
+            </div>
+        </section>
+        <!-- Módulo de Facturación -->
+        <section class="modulo" id="facturacion">
+            <div class="modulo-header">Facturación</div>
+            <div class="modulo-content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Cedula</th>
+                            <th>Descripción</th>
+                            <th>Valor</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Aquí puedes agregar filas con datos de facturación -->
+                        <tr>
+                            <td>123456789</td>
+                            <td>Factura 1</td>
+                            <td>$100.00</td>
+                            <td>
+                                <button class="crear-button">crear</button>
+                                <button class="modificar-button">Modificar</button>
+                                <button class="eliminar-button">Eliminar</button>
+                            </td>
+                        </tr>
+                        <!-- Agrega más filas según sea necesario -->
+                    </tbody>
+                </table>
+            </div>
+        </section>
+        <!-- Modulo de Habitaciones -->
+        <section class="modulo" id="habitaciones">
+            <div class="modulo-header">Habitaciones</div>
+            <div class="modulo-content">
+                <!-- Contenido del modulo de habitaciones... -->
+                <p>Contenido del módulo de habitaciones...</p>
+            </div>
+        </section>
+
+        <!-- Modulo de Huéspedes -->
+        <section class="modulo" id="huespedes">
+            <div class="modulo-header">Huéspedes</div>
+            <div class="modulo-content">
+                <!-- Contenido del módulo de huéspedes... -->
+                <p>Contenido del módulo de huéspedes...</p>
+            </div>
+        </section>
+
+        <!-- Módulo de Pedidos -->
+        <section class="modulo" id="pedidos">
+            <div class="modulo-header">Pedidos</div>
+            <div class="modulo-content">
+                <!-- Contenido del módulo de pedidos... -->
+                <p>Contenido del módulo de pedidos...</p>
+            </div>
+        </section>
+
+        <!-- Módulo de Usuarios -->
+        <section class="modulo" id="usuarios">
+            <div class="modulo-header">Usuarios</div>
+            <div class="modulo-content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Documento</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Correo</th>
+                            <th>Teléfono</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<tr>';
+                                echo '<td>' . $row['documento'] . '</td>';
+                                echo '<td>' . $row['nombre'] . '</td>';
+                                echo '<td>' . $row['apellido'] . '</td>';
+                                echo '<td>' . $row['correo'] . '</td>';
+                                echo '<td>' . $row['telefono'] . '</td>';
+                                echo '<td>';
+                                echo '<button class="crear-button">crear</button>';
+                                echo '<button class="modificar-button">Modificar</button>';
+                                echo '<button class="eliminar-button">Eliminar</button>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        } else {
+                            echo '<tr><td colspan="4">No hay registros disponibles.</td></tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+        <!-- Módulo de Configuración -->
+        <section class="modulo" id="configuracion">
+            <div class="modulo-header">Configuración</div>
+            <div class="modulo-content">
+                <!-- Contenido del módulo de configuración... -->
+                <p>Contenido del módulo de configuración...</p>
+            </div>
+        </section>
+    </main>
+
+    <!-- Pie de página del panel de gestión -->
+    <footer>
+        <p>&copy; 2023 Panel de Gestión. Todos los derechos reservados.</p>
+    </footer>
+    <script src="js/panel.js"></script>
+</body>
+
+</html>
