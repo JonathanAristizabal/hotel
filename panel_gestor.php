5198608baf5d17a6a96d2b09eac89fdd8d30b37b
JonathanAristizabal <jhonatan901230@hotmail.com>
2023-08-23 20:49:38 -0500

    se crea pag reservas y css

--- panel_gestor.php
+++ panel_gestor.php
@@ -25,6 +25,7 @@ $result = $conn->query($sql);
         <nav class="navegacion">
             <ul>
                 <li><a href="index.php">Inicio</a></li>
+                <li><a href="reservas.php">Reservas</a></li>
                 <li><a href="logout.php">Cerrar sesión</a></li>
             </ul>
         </nav>
